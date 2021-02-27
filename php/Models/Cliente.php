e5770d2d7d1338d7ccf736ed5e9fe37e67cdb9b2
Jose Luis Capote <jcapoterojas30@gmail.com>
2021-02-26 21:15:41 -0500

    Tercer commit

---
+++ php/Models/Cliente.php
@@ -0,0 +1,15 @@
+<?php
+require_once("../Models/Empresas.php");
+class  Cliente{
+  private $idCliente;
+  private $telefono;
+  private $celular;
+  private $direccion;
+  private $login;
+  private $password;
+  private $email;
+  private $detalleAdicional;
+  private $fechaRegistro;
+  private $fechaModificacion;
+}
+?>

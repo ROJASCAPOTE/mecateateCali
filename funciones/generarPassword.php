a5b29ccbf7bf2121c6acca788167979991361f97
Jose Luis Capote <jcapoterojas30@gmail.com>
2021-03-22 16:03:29 -0500

    Template

--- funciones/generarPassword.php
+++ funciones/generarPassword.php
@@ -1,9 +1,10 @@
 <?php
-    $opciones = [
-        'cost' => 12,
-    ];
-    $pass = $_GET["pass"];
-    $passwordHashed = password_hash($pass, PASSWORD_BCRYPT, $opciones);
 
-    echo $passwordHashed;
+$opciones = [
+    'cost' => 12,
+];
+$pass = $_GET["pass"];
+$passwordHashed = password_hash($pass, PASSWORD_BCRYPT, $opciones);
+
+echo $passwordHashed;
 ?>
